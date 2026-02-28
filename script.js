ffcacb91576b8b9318fb3993bce311d662355e95
pradhiksha766 <pradhiksha766@gmail.com>
2026-02-28 09:39:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll enhancement (optional extra control)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
